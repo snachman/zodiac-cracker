f887ea3758770b82002915f7d92d1c20c2e204c0
sam <shnachman@gmail.com>
2020-12-13 21:10:14 -0500

    create data gen

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 
 
+def generate_symbols():
+    for i in range(100):
+        print("'" + str(i) + "symbol'" + ", ", end="")
+
+
 
 if __name__ == '__main__':
-    pass
+
+    dat = ['0symbol', '1symbol', '2symbol', '3symbol', '4symbol', '5symbol', '6symbol', '7symbol', '8symbol', '9symbol', '10symbol', '11symbol', '12symbol', '13symbol', '14symbol', '15symbol', '16symbol', '17symbol', '18symbol', '19symbol', '20symbol', '21symbol', '22symbol', '23symbol', '24symbol', '25symbol', '26symbol', '27symbol', '28symbol', '29symbol', '30symbol', '31symbol', '32symbol', '33symbol', '34symbol', '35symbol', '36symbol', '37symbol', '38symbol', '39symbol', '40symbol', '41symbol', '42symbol', '43symbol', '44symbol', '45symbol', '46symbol', '47symbol', '48symbol', '49symbol', '50symbol', '51symbol', '52symbol', '53symbol', '54symbol', '55symbol', '56symbol', '57symbol', '58symbol', '59symbol', '60symbol', '61symbol', '62symbol', '63symbol', '64symbol', '65symbol', '66symbol', '67symbol', '68symbol', '69symbol', '70symbol', '71symbol', '72symbol', '73symbol', '74symbol', '75symbol', '76symbol', '77symbol', '78symbol', '79symbol', '80symbol', '81symbol', '82symbol', '83symbol', '84symbol', '85symbol', '86symbol', '87symbol', '88symbol', '89symbol', '90symbol', '91symbol', '92symbol', '93symbol', '94symbol', '95symbol', '96symbol', '97symbol', '98symbol', '99symbol', ]
+
